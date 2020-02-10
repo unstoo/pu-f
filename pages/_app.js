29ea1c7e37706da3e0c67e8368cbeea42ac64d37
Yevgeny <unstoo@gmail.com>
2020-02-10 23:57:18 +0300

    feature: EmailInput;

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import 'react-phone-input-2/lib/style.css'
 import '../style.css'
 import '../SMSCodeInput.css'
 import '../PasswordInput.css'
+import '../EmailInput.css'
 import '../AccountTypeInput.css'
 
 // This default export is required in a new `pages/_app.js` file.
